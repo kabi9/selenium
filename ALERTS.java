42f360c703f38889c813b374e40406e97453dd42
kabi9 <innocentkabi95@gmail.com>
2022-10-18 21:59:46 -0500

    Links Alerts

---
+++ ALERTS.java
@@ -0,0 +1,32 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ALERTS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Asukabi\\OneDrive\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+	
+		WebDriver driver = new ChromeDriver();		
+		
+		driver.get("http://www.webdriveruniversity.com/Popup-Alerts/index.html");
+		
+		driver.findElement(By.id("button1")).click();
+		driver.switchTo().alert().accept();
+		
+		driver.findElement(By.id("button4")).click();
+		driver.switchTo().alert().accept();
+		//driver.switchTo().alert().dismiss();
+		
+		
+	}
+	
+
+}
+
